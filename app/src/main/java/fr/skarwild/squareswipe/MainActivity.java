197b4b23e590facb465153f16b47d8140eefaebf
Nicolas99-9 <bougie.nicolas@gmail.com>
2016-06-29 22:41:02 +0200

    Add the files

---
+++ app/src/main/java/fr/skarwild/squareswipe/MainActivity.java
@@ -0,0 +1,35 @@
+package fr.skarwild.squareswipe;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    private gameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new gameView(this);
+        setContentView(gameView);
+    }
+
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        gameView.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        gameView.pause();
+    }
+}
